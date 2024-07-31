c1f898a3aab521c13799014b6e35a7efbbc05914
Gavin OMelia <gavin@omelia.org>
2024-07-30 21:30:02 -0700

    Handle blank URLs

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,6 +3,8 @@ class Recipe < ApplicationRecord
   has_many :ingredients, dependent: :destroy
   accepts_nested_attributes_for :ingredients, allow_destroy: true
 
-  validates :name, :url, :directions, :user, presence: true
+  validates :name, :directions, :user, presence: true
+
+  validates :url, format: { with: URI::regexp(%w[http https]) }, allow_blank: true
 
 end
