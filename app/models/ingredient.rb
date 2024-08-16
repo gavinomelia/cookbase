097857018e1548506a089bb3e25871a0463f8a14
Gavin OMelia <gavin@omelia.org>
2024-08-15 21:44:55 -0700

    Properly handle unicode fraction characters
    - Adjust ingredient model to transform fraction characters to floats
    - Currently all quantities are converted to flaots
    - Solves #14

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -8,11 +8,43 @@ class Ingredient < ApplicationRecord
 
   def extract_quantity
    # Matches fractions, integers, and decimals at the start of a string.
-    regex = %r{\A(\d+/\d+|\d+(\.\d+)?)}
+    regex = %r{\A(\d+/\d+|\d+(\.\d+)?|[¼½¾⅓⅔⅕⅖⅗⅘⅙⅚⅛⅜⅝⅞])}
 
     if match = self.name.match(regex)
-      self.quantity = match[0].to_f
-     else self.quantity = 1
+      floatify(match[0])
+    else 
+      self.quantity = 1
     end
   end
+
+  def floatify(str)
+    if fraction_to_float(str) != nil
+      self.quantity = fraction_to_float(str)
+    else
+      self.quantity = str.to_f
+    end
+  end
+
+  def fraction_to_float(str)
+    conversion = {
+      '¼' => 0.25,
+      '½' => 0.5,
+      '¾' => 0.75,
+      '⅓' => Rational(1, 3).to_f.round(2),
+      '⅔' => Rational(2, 3).to_f.round(2),
+      '⅕' => 0.2,
+      '⅖' => 0.4,
+      '⅗' => 0.6,
+      '⅘' => 0.8,
+      '⅙' => Rational(1, 6).to_f.round(2),
+      '⅚' => Rational(5, 6).to_f.round(2),
+      '⅛' => 0.125,
+      '⅜' => 0.375,
+      '⅝' => 0.625,
+      '⅞' => 0.875
+    }
+
+    conversion[str]
+  end
+
 end
