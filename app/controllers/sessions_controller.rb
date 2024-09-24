95119a4a1742564375f53ff45f33ae5c3ba08525
Gavin OMelia <gavin@omelia.org>
2024-09-23 17:23:01 -0700

    Fix notice for improper email/password combo

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,8 +8,7 @@ class SessionsController < ApplicationController
     if user
       redirect_to root_path, notice: 'Logged in successfully.'
     else
-      flash.now[:alert] = 'Invalid email or password.'
-      render :new
+      redirect_to login_path, notice: 'Invalid email or password.'
     end
   end
 
