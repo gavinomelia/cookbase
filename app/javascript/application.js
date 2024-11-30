16c6de5285a066dbb73b755bb6eae4f9b4bcd015
Gavin OMelia <gavin@omelia.org>
2024-11-30 09:56:29 -0800

    Fix some syntax in importmaps and imports in application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails";
 import "controllers";
-import "./features/instant_search";
-import "./features/tags";
-import "./features/ingredients";
-import "./features/image_preview";
-import "./features/tabs";
-import "./features/mobile_menu_search";
-import "./features/update_scale";
+import "features/instant_search";
+import "features/tags";
+import "features/ingredients";
+import "features/image_preview";
+import "features/tabs";
+import "features/mobile_menu_search";
+import "features/update_scale";
